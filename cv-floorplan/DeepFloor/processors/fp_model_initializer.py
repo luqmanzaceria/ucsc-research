7951d2c446f5775bf2781bebdc1a7f8fb603ae96
Luqman Zaceria <luqmanzaceria@gmail.com>
2023-10-20 12:57:57 -0700

    Added DeepFloor code

---
+++ cv-floorplan/DeepFloor/processors/fp_model_initializer.py
@@ -0,0 +1,15 @@
+import os
+
+from deepfloor.net import *
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
+
+def init_model(path_weights):
+    # initialize the model
+    floor_plan_model = DeepFloorplanModel()
+
+    # restore weights from previous training
+    floor_plan_model.load_weights(path_weights)
+
+    return floor_plan_model
