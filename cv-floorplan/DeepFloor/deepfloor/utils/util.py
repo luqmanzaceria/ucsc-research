e55b20957494b9058f314ac5f58f211413c15b4a
Luqman Zaceria <luqmanzaceria@gmail.com>
2023-11-07 20:35:49 -0800

    Make deepfloor code work on mac
    numpy int adjustment

--- cv-floorplan/DeepFloor/deepfloor/utils/util.py
+++ cv-floorplan/DeepFloor/deepfloor/utils/util.py
@@ -14,13 +14,13 @@ def flood_fill(test_array, h_max=255):
 	fill in the hole 
 	"""
 	input_array = np.copy(test_array) 
-	el = ndimage.generate_binary_structure(2,2).astype(np.int)
+	el = ndimage.generate_binary_structure(2,2).astype(np.int64)
 	inside_mask = ndimage.binary_erosion(~np.isnan(input_array), structure=el)
 	output_array = np.copy(input_array)
 	output_array[inside_mask]=h_max
 	output_old_array = np.copy(input_array)
 	output_old_array.fill(0)   
-	el = ndimage.generate_binary_structure(2,1).astype(np.int)
+	el = ndimage.generate_binary_structure(2,1).astype(np.int64)
 	while not np.array_equal(output_old_array, output_array):
 		output_old_array = np.copy(output_array)
 		output_array = np.maximum(input_array,ndimage.grey_erosion(output_array, size=(3,3), footprint=el))
