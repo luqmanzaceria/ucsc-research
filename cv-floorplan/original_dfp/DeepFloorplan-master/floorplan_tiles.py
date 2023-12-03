621bd8d47de844e5e1f06afa6c59ff43c3d363e9
Luqman Zaceria <luqmanzaceria@gmail.com>
2023-12-03 13:21:48 -0800

    Fixed issue with tile assembly
    Given an input folder of images, all final reassembled images would contain the assembled tiles from only the first inputted image. The script was not handling tile assembly from each image in the input folder properly.

--- cv-floorplan/original_dfp/DeepFloorplan-master/floorplan_tiles.py
+++ cv-floorplan/original_dfp/DeepFloorplan-master/floorplan_tiles.py
@@ -35,33 +35,22 @@ def resize_tile(tile_img, new_width, new_height):
     # Resize the tile image to the new dimensions
     return cv2.resize(tile_img, (new_width, new_height), interpolation=cv2.INTER_LINEAR)
 
-def join_images(dir_out, original_width, original_height, tile_size):
+def join_images(dir_out, original_width, original_height, tile_size, image_prefix):
     # Create a new blank image with the original dimensions
     full_image = np.zeros((original_height, original_width), dtype=np.uint8)
 
-    # Initialize a variable to store the prefix
-    prefix = None
-
-    # Compile a regular expression pattern to detect the file pattern
-    file_pattern = re.compile(r"(.*?)_tile_(\d+)_(\d+).png")
-
-    # Scan directory for files and determine the prefix
-    for filename in os.listdir(dir_out):
-        match = file_pattern.match(filename)
-        if match:
-            prefix, y, x = match.groups()
-            break  # We just need to detect the prefix once
-
-    if prefix is None:
-        raise ValueError("No matching files found for the given pattern.")
-
-    # Update the tile pattern to include the detected prefix
-    tile_pattern = re.compile(rf"{re.escape(prefix)}_tile_(\d+)_(\d+).png")
+    # Adjust the regular expression to match the file naming convention
+    # This pattern assumes the format 'binary_[image_prefix]_tile_[y]_[x].png'
+    tile_pattern = re.compile(rf"binary_{re.escape(image_prefix)}_tile_(\d+)_(\d+).png")
 
     # Dictionary to hold tile images
     tiles = {}
 
-    # Scan directory for matching files using the new pattern
+    print("=========================")
+    print(f"image_prefix: {image_prefix}")
+    print("=========================")
+
+    # Scan directory for matching files using the updated pattern
     for filename in os.listdir(dir_out):
         match = tile_pattern.match(filename)
         if match:
@@ -73,16 +62,12 @@ def join_images(dir_out, original_width, original_height, tile_size):
     # Iterate over the expected tile positions
     for y in range(0, original_height, tile_size):
         for x in range(0, original_width, tile_size):
-            # Calculate new tile size considering the edge cases
-            new_tile_width = min(tile_size, original_width - x)
-            new_tile_height = min(tile_size, original_height - y)
-
             # Check if the tile image exists in the dictionary
             if (y, x) in tiles:
                 # Resize the tile to the new size
-                resized_tile_img = resize_tile(tiles[(y, x)], new_tile_width, new_tile_height)
+                resized_tile_img = resize_tile(tiles[(y, x)], min(tile_size, original_width - x), min(tile_size, original_height - y))
                 # Place the resized tile into the full image
-                full_image[y:y + new_tile_height, x:x + new_tile_width] = resized_tile_img
+                full_image[y:y + min(tile_size, original_height - y), x:x + min(tile_size, original_width - x)] = resized_tile_img
 
     return full_image
 
@@ -116,7 +101,7 @@ def main():
                 binary_path = os.path.join(binary_folder, "binary_" + os.path.basename(tile_path))
                 output_image_path = os.path.join(args.output_folder, "processed_" + os.path.basename(tile_path))
 
-                process_tiles.process_imgNEWALG(tile_path, output_image_path, mode='RGB')
+                process_tiles.process_img(tile_path, output_image_path, mode='RGB')
 
                 # Check dimensions after processing
                 processed_img = Image.open(output_image_path)
@@ -136,8 +121,10 @@ def main():
                 final_height, final_width = final_img.shape[:2]
                 print(f"Final binary image {os.path.basename(binary_path)} dimensions (Width x Height): {final_width} x {final_height}")
 
+            # Extract the prefix from the floor plan name
+            image_prefix = os.path.splitext(floor_plan_name)[0]
             # Reassemble the final binary images into a single image
-            full_binary_image = join_images(binary_folder, original_width, original_height, tile_size)
+            full_binary_image = join_images(binary_folder, original_width, original_height, tile_size, image_prefix)
                 
             # Save the reassembled image
             reassembled_path = os.path.join(args.output_folder, "reassembled", f"reassembled_{floor_plan_name}")
